318b3920637a43561cf8e763019f52b620f30464
ayushr-1o <ayush.raj@alumni.esade.edu>
2026-04-05 21:26:59 +0200

    Fix CORS trailing slash

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://timetravel-frontend.up.railway.app/"  
+        "https://timetravel-frontend.up.railway.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
